1cac0cba3de8e1b55b7350f5cdefea0fb03df0cd
ivo692009 <ivo692009@gmail.com>
2017-11-08 15:12:41 -0300

    ahora si el CRUD hecho

--- routes/web.php
+++ routes/web.php
@@ -23,18 +23,19 @@ Route::get('/tipos', 'tiposController@index');
 //Metodos para los 3 abms
 //descriptores
 Route::get('/descriptores/nuevo', 'descriptoresController@create');
-Route::get('/descriptores/modificar/{id}','descriptoresController@show');
+Route::get('/descriptores/modificar/{id}','descriptoresController@show')->name('/Modificar');
 Route::post('/descriptores/modificado','descriptoresController@update');
 Route::get('/descriptores/eliminar/{id}','descriptoresController@destroy');
 Route::post('/descriptores/crear','descriptoresController@store');
 //tipos
 Route::get('/tipos/nuevo', 'tiposController@create');
-Route::get('/tipos/modificar/{id}','tiposController@show');
+Route::get('/tipos/modificar/{id}','tiposController@show')->name('/Modificar');
 Route::post('/tipos/modificado','tiposController@update');
 Route::get('/tipos/eliminar/{id}','tiposController@destroy');
 Route::post('/tipos/crear','tiposController@store');
 //estados
 Route::get('/estados/nuevo', 'estadoController@create');
-Route::get('/estados/modificar/{id}','estadoController@show');
+Route::get('/estados/modificar/{id}','estadoController@show')->name('/Modificar');
+Route::post('/estados/modificado','estadoController@update');
 Route::get('/estados/eliminar/{id}','estadoController@destroy');
 Route::post('/estados/crear','estadoController@store');
